1571020b8f8d07e1813ba18bcba5319d35eb6044
ebatz12965 <ebatzm@miumg.edu.gt>
2024-05-14 12:42:14 -0600

    se agregó la vista y modelo de Clientes

---
+++ Taller/Pages/Clientes.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Taller.Pages;
+
+public class ClienteModel : PageModel
+{
+    private readonly ILogger<ClienteModel> _logger;
+    
+    public ClienteModel(ILogger<ClienteModel> logger)
+    {
+        _logger = logger;
+    }
+    public void OnGet()
+    {
+        
+    }
+}
