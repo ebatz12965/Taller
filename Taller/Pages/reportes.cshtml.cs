455c40d8261eda35419a55f12de161d47bfeeda9
ebatz12965 <ebatzm@miumg.edu.gt>
2024-05-13 21:44:09 -0600

    primer commit

---
+++ Taller/Pages/reportes.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taller.Pages;
+
+public class reportes_cshtml : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
